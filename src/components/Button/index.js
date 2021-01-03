c506fde7880c95d3fb11cc5ba72283b4943940f4
Gilles <lorison.gilles@luminae.com.br>
2021-01-03 18:19:56 -0300

    Create complete structure and styling

---
+++ src/components/Button/index.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+// function Button() {
+//   return Ola;
+// }
+
+// export default Button;
---
+++ src/components/FormSignUpLogin/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import Box from '../Box';
+
+const FormSignUpLogin = styled(Box).attrs({ as: 'form' })`
+  width: 600px;
+  height: 420px;
+`;
+
+export default FormSignUpLogin;
